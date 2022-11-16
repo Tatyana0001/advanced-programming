f655877299b8e20a1587202f1254fc7f80e841e3
Татьяна Кочубейник <kisa7392@mail.ru>
2022-11-16 20:10:47 +0300

    First version

---
+++ Lesson 1/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,26 @@
+﻿// Task 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::vector<int> v = { 4, 7, 9, 14, 12 };
+    std::cout << "Входные данные: ";
+    for (int n : v) {
+        std::cout << n << " ";
+    }
+    std::cout << "\n";
+    std::cout << "Выходные данные: ";
+    std::for_each(v.begin(), v.end(), [](int n) {
+        if (n % 2 == 1) { n *= 3; }
+        std::cout << n << " "; });
+    std::cout << "\n";
+    return 0;
+}
+
+
---
+++ Lesson 1/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,41 @@
+﻿// Task 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <variant>
+//#include <vector>
+//#include <algorithm>
+//#include <tuple>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+auto get_var(auto result) {
+	auto get_int = std::get<int>(result)
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+}
+
+
