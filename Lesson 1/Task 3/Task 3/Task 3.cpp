396eb27e68ccfaad0d9c5e98fade7dd41aeb2c07
Татьяна Кочубейник <kisa7392@mail.ru>
2022-12-01 21:13:26 +0300

    Third version

--- Lesson 1/Task 3/Task 3/Task 3.cpp
+++ Lesson 1/Task 3/Task 3/Task 3.cpp
@@ -7,13 +7,17 @@
 #include <vector>
 #include <algorithm>
 
-void SinCos(double a) {
-	a = a * 3.1415926 / 180;
-	std::cout << a << ": sin: " << sin(a) << " cos: " << cos(a);
+void Sin(double a) {
+	std::cout << "sin: " << sin(a) << " ";
+}
+
+void Cos(double a) {
+	std::cout << "cos: " << cos(a);
 }
 
 void Print(std::vector<double> &angles, std::vector<std::function<void(double)>> functions) {
 	for (const auto& angle : angles) {
+		std::cout << angle << ": ";
 		for (const auto& function : functions)
 			function(angle);
 		std::cout << std::endl;
@@ -28,8 +32,12 @@ int main(){
 		std::cout << n << " * 3.1415926 / 180" << "\n";
 		});
 	std::cout << "[Выходные данные]:\n";
+	for (double &angle : a) {
+		angle = angle * 3.1415926 / 180;
+	}
 	std::vector<std::function<void(double)>> fVector;
-	fVector.emplace_back(SinCos);
+	fVector.emplace_back(Sin);
+	fVector.emplace_back(Cos);
 	Print(a, fVector);
 	return 0;
 }
