cba164127ca3652a5a474c3b2f8a1c05e4c83474
Татьяна Кочубейник <kisa7392@mail.ru>
2023-02-01 21:04:02 +0300

    First version

---
+++ Lesson 7/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,31 @@
+﻿// Task 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void sort(std::vector<int>& v) {
+    std::sort(v.begin(), v.end());
+    auto it = std::unique(v.begin(), v.end());
+    v.erase(it, v.end());
+}
+
+int main()
+{
+    std::vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    auto it = v.begin();
+    while (it != v.end()) {
+        std::cout << *it << " ";
+        it++;
+    }
+    std::cout << "\n";
+    sort(v);
+    std::cout << "[OUT]: ";
+    for (int i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+    return 0;
+}
---
+++ Lesson 7/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,30 @@
+﻿// Task 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+
+template<class T>
+void print_container(std::string str, T& a) {
+    std::cout << str << "\n";
+    for (auto const& n : a) {
+        std::cout << n << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container("set", test_set); 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container("list", test_list); 
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container("vector", test_vector); 
+    
+    return 0;
+}
