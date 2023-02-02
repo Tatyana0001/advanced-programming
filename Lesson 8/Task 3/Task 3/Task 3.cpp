13262410517517a5f2fde681a3eebe916daae87e
Татьяна Кочубейник <kisa7392@mail.ru>
2023-02-02 19:14:54 +0300

    First version

---
+++ Lesson 8/Task 3/Task 3/Task 3.cpp
@@ -0,0 +1,32 @@
+﻿// Task 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+template<class T>
+class unique_ptr
+{
+private:
+    T* ptr;
+public:
+    unique_ptr(T* arr) : ptr(arr) {}
+    T& operator*() { return *ptr; }
+    unique_ptr(const unique_ptr&) = delete;
+    unique_ptr& operator=(const unique_ptr&) = delete;
+    T release() {
+        T* ptr2 = ptr;
+        ptr = nullptr;
+        return ptr2;
+    }
+    ~unique_ptr() {
+        delete[] ptr;
+        std::cout << "destructor called\n";
+    }
+};
+
+
+int main() {
+    int* arr = new int[100];
+    unique_ptr<int> my_ptr(arr);
+    return 0;
+} 
